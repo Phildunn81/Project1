1a016c2a381518c06270d29983794caf979fee19
phildunn81 <phillipdunn@hotmail.com>
2020-06-10 23:26:18 +0100

    final score added

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
 const pack = document.querySelectorAll('.card');
 const cards = [...pack];
 const moves = document.getElementById('moves');
-const restart= document.getElementById('restart');
+const restart = document.getElementById('restart');
+
 // shuffle cards
+let match = 1;
+let totalMatched = [];
 let movesCount = 0;
-let openedCards = []
+let openedCards = [];
 // click cards show, stay shown, close
 const cardOpen =(card) => {
     openedCards.push(card);
-    console.log(openedCards)
     let length = openedCards.length;
     if(length === 2) {
         moveCounter();
         if (openedCards[0].alt === openedCards[1].alt) {
             matched();
+            totalMatched.push(match);
         } else {
             unmatched();
         }
@@ -53,32 +56,33 @@ let moveCounter = () => {
 
 
 
-// const shuffle = (cards) => {
-//     let currentIndex = cards.length, temporaryValue, randomIndex;
-//     while (currentIndex !== 0) {
-//       randomIndex = Math.floor(Math.random() * currentIndex);
-//       currentIndex -= 1;
-//       temporaryValue = cards[currentIndex];
-//       cards[currentIndex] = cards[randomIndex];
-//       cards[randomIndex] = temporaryValue;
-//     }
-//     return cards;
-//   }
-//   const deck = document.querySelector(".deck");
-// const beginGame = () => {
-//    let shuffledCards = shuffle(cards);
-//    for (let i= 0; i < shuffledCards.length; i++){
-//       [].forEach.call(shuffledCards,(item) => {
-//          deck.appendChild(item);
-//       });
-//    }
-// }
+const shuffle = (cards) => {
+    let currentIndex = cards.length, temporaryValue, randomIndex;
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = cards[currentIndex];
+      cards[currentIndex] = cards[randomIndex];
+      cards[randomIndex] = temporaryValue;
+    }
+    return cards;
+  }
+  const deck = document.querySelector(".deck");
+const beginGame = () => {
+   let shuffledCards = shuffle(cards);
+   for (let i= 0; i < shuffledCards.length; i++){
+      [].forEach.call(shuffledCards,(item) => {
+         deck.appendChild(item);
+      });
+   }
+}
 
 
 // start game and timer when window open
 const startGame =()=> {
-    const zero = 0,
+    let zero = 0,
     display = document.getElementById('timer');
+    movesCount = 0;
     startTimer(zero, display);
     // beginGame();
  };
@@ -96,18 +100,20 @@ const startTimer = (duration, display) => {
     }, 1000);
 }
 
+    
 //restart game
 
-const restartGame = () =>{restart.addEventListener('click',(event) =>{
-     startGame();
-   })}
+const restartGame = () =>{restart.addEventListener('click',startGame())};
    
 
 // final score
+let finalScore = () => {
+    if (totalMatched.length = 8)
+    document.querySelector('congrats').classList.toggle('finish');
+    document.createTextNode(`Congrats you finished in ${'movesCount.textContet'} moves and a time of ${'timer'}`);
+    console.log("yay");
+};
 
-const finalScore = () =>{
-    if (matched);
-}
 window.onload= startGame();
 
  //pause
