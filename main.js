3e15d57bc8ed149d47d20c3c3e7c563368aceb94
phildunn81 <phillipdunn@hotmail.com>
2020-06-20 20:59:43 +0100

    modal added, working

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const cards = [...pack];
 const moves = document.getElementById('moves');
 const restart = document.getElementById('restart');
 const display = document.getElementById('timer');
-const modal = document.querySelector('modal');
+const modal = document.getElementById('modal');
+const modalText = document.getElementById('modal-text');
+const span = document.getElementsByClassName("close")[0];
 
 const shopkinsImage = {
     card1: "./images/banan.jpg",
@@ -16,16 +18,23 @@ const shopkinsImage = {
     card8: "./images/donut.jpg",
     card9: "./images/toast.jpg",
     card10: "./images/strawberry.jpg",
-    // card11: "./images/pineapple.jpg",
-    // card12: "./images/cupcake.jpg",
-}
+};
 
 let openedCards = [];
 let totalMatched = [];
 let movesCount = 0;
 let timeout;
 let zero = 0;
-// loop through cards
+//loops through cards and asigns a eventlistner that triggers image change and the cardopen logic below
+for (const card of cards) {card.addEventListener('click', (event) =>{
+    showCard(event);
+    cardOpen(event.target);
+   })}
+//matches the object of images with the alt description in html for the cards thats targeted to show the card
+const showCard = (event) => { 
+    event.target.src = shopkinsImage[event.target.alt]
+};
+// loops through the cards, checking for matches of 2
 const cardOpen =(card) => {
     openedCards.push(card);
     let length = openedCards.length;
@@ -35,40 +44,29 @@ const cardOpen =(card) => {
             matched();
             totalMatch();
         } else {
-            unmatched();
+        unmatched();
         }
     }
 };
-//matched function
+//matched function, toggles CSS  
 const matched = () => {
     openedCards[0].classList.toggle('matched');
     openedCards[1].classList.toggle('matched');
     openedCards = [];
-}
-//unmatched function with a delay
+}; 
+//unmatched function with a delay, 
 const unmatched= () => {
     setTimeout(() => {
     openedCards[0].src= "./images/shopkin.jpg";
     openedCards[1].src= "./images/shopkin.jpg";
     openedCards = [];
-},400);
-}
-//matches the object of images with the alt description in html for the cards thats targeted
-const showCard = (event) => { 
-    event.target.src = shopkinsImage[event.target.alt]
-    // event.target.classList.toggle('test');
-}
-//loops through cards and asigns a eventlistner that triggers image change and the cardopen logic above
-for (const card of cards) {card.addEventListener('click', (event) =>{
- showCard(event);
- cardOpen(event.target);
-})}
+},300);
+};
 //counts the moves stored in moves count variable and displayed in moves html
 const moveCounter = () => {
     movesCount ++;
     moves.innerHTML= movesCount;
-}
-
+};
 // shuffles card
 const shuffle = (cards) => {
     let currentIndex = cards.length, temporaryValue, randomIndex;
@@ -78,78 +76,83 @@ const shuffle = (cards) => {
       temporaryValue = cards[currentIndex];
       cards[currentIndex] = cards[randomIndex];
       cards[randomIndex] = temporaryValue;
-    }
+    };
     return cards;
-}
+};
 // puts shuffled cards back into the html grid
 const gameArea = document.querySelector(".cards");
 const cardsInHtml = (cards) => {
     gameArea.innerHTML = "";
     cards.forEach(card => {
         gameArea.appendChild(card);
-    })
-}
+    });
+};
 // starts the game off
 const startGame =()=> {
-    // cards.forEach(()card.);
     moves.innerHTML =0;
     clearInterval(timeout);
     startTimer(zero, display);
     beginGame();  
     changeImageBack();
 };
-
+// changes images back to original
 const changeImageBack = () => {
     cards.forEach(card=> { 
         card.src = "./images/shopkin.jpg";
     });
 };
-   // 
+   // shuffles cards at the start of the game
 const beginGame = () => {
     let shuffledCards = shuffle(cards);
      cardsInHtml(shuffledCards);
-}
- // strats the timer
+};
+ // starts the timer
 const startTimer = (duration, display) => {
     let timer = duration, minutes, seconds;
     timeout = setInterval( () => {
         minutes = parseInt(timer / 60, 10);
         seconds = parseInt(timer % 60, 10);
-        minutes = minutes < 10 ? "0" + minutes : minutes;
+        minutes = minutes < 10 ? minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
         display.textContent = minutes + ":" + seconds;
         if (++ timer < 0) {
             timer = duration;
         }
     }, 1000);
-}
-
-    const totalMatch = (match) => {
-        totalMatched.push(match);
-        let leng = totalMatched.length;
-        if(leng > 0) { 
-            modal.style.display = "block";
-        }
-        // (`Congratulations it took you this time ${display.textContent} and ${movesCount} moves to complete`);
 };
-
+//Pops out a modal when the game is over
+ const totalMatch = (match) => {
+    totalMatched.push(match);
+    let leng = totalMatched.length;
+    if(leng >= 10) { 
+        modal.style.display = "block";
+        modalText.innerHTML = `Congratulations!!\n Your time was ${display.textContent} and it took you ${movesCount} moves to complete!`;
+    }
+};
+// click to close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+};
+  
+// click outside modal to close modal
 window.onclick = function(event) {
     if (event.target == modal) {
       modal.style.display = "none";
     }
 };
-
-    
-    // document.createTextNode(`Congrats you finished in ${'movesCount.textContet'} moves and a time of ${'timer'}`);
+  
     // document.getElementById("myAudio");
     // new Audio
 
-
+// restart button
 restart.addEventListener('click', startGame);
-
-
+// calls the start game function
 startGame();
 
- //pause
+
+
+
+
+//pause
 // const textWrapper = document.querySelector('.title');
 // textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
